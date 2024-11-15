c470a358633021f2339053a4d6f6166bb83eecb8
Rajeev <126416818+rajeev-27@users.noreply.github.com>
2024-11-15 23:49:07 +0530

    Python

---
+++ Day-8/positional_argument/main.py
@@ -0,0 +1,10 @@
+def get(name, location):
+    print(f"Hello {name}")
+    print(f"Your location is {location}")
+get("Rajeev", "Noida")
+
+
+def go(name, location):
+    print(f"Hello {name}")
+    print(f"Your location is {location}")
+go(name = "Anurag", location = "Delhi")

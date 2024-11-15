c9c75b711d569d1793cf38470eb50119a377192a
Rajeev <126416818+rajeev-27@users.noreply.github.com>
2024-11-15 23:32:05 +0530

    Python

---
+++ Day-8/function/main.py
@@ -0,0 +1,14 @@
+def hello():
+    print("Hello Rajeev")
+    print("HOw do you do Anurag")
+    print("is not the weather nice today?")
+
+hello()
+
+name = input("What is you name? ")
+def hi(name):
+    print(f"Hello {name}")
+    print(f"HOw do you do {name}")
+    print("is not the weather nice today?")
+
+hi(name)

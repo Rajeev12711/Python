b8c593883146dd9afb299f501abda5d53b773b09
Rajeev <126416818+rajeev-27@users.noreply.github.com>
2024-11-20 23:47:09 +0530

    Python

---
+++ Day-9/dictionary/main.py
@@ -0,0 +1,7 @@
+dictionary = {
+    "bug": "this is problem", 
+    "function": "recall the function"}
+dictionary["loop"] = "for and whille"
+
+print(dictionary["bug"])
+print(dictionary)
---
+++ Day-9/nesting_dict/main.py
@@ -0,0 +1,13 @@
+dictionary = [
+    {
+        "country": "spain",
+        "city": ["madrid","lugo"],
+        "total_guest": 10
+    },
+    {
+        "county": "japan",
+        "city": ["tohoku", "kanto"],
+        "total_guest": 5
+    },
+    ]
+print(dictionary)

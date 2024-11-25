37db4262f793807a10aeef8cabd486e342aee71f
Rajeev <126416818+rajeev-27@users.noreply.github.com>
2024-11-25 23:56:08 +0530

    Python

--- Day-10/exercise/main2.py
+++ Day-10/exercise/main2.py
@@ -5,7 +5,9 @@ def subtract(x, y):
 def multiple(x, y):
     return x*y
 def divide(x, y):
-    return x/y
+    if y == 0:
+        return "Error: Division by zero is not allowed."
+    return x / y
 def module(x, y):
     return x%y
 
@@ -16,20 +18,25 @@ operations ={
     "/":divide,
     "%": module
     }
-x = float(input("enter the first number: "))
-for operator in operations:
-    print(operator)
-symbol = input("Choose Any Operation From the Line Above: ")
-y = float(input("enter the second number: "))
-calculate = operations[symbol]
-answer = calculate(x, y)
 
-print(f"{x} {symbol} {y} = {answer}")
-x = answer
-for operator in operations:
-    print(operator)
-symbol = input("Choose Any Operation From the Line Above: ")
-z = float(input("enter the third number: "))
-calculate = operations[symbol]
-answer = calculate(x, z)
-print(f"{x} {symbol} {z} = {answer}")
+x = float(input("enter the first number: "))
+again = True
+while again == True:
+    for operator in operations:
+        print(operator)
+    symbol = input("Choose Any Operation From the Line Above: ")
+    y = float(input("enter the next number: "))
+    calculate = operations[symbol]
+    answer = calculate(x, y)
+
+    print(f"{x} {symbol} {y} = {answer}")
+    x = answer
+    
+    reuse = input("Type 'yes' to continue calculating with  {answer}, or type 'no' to Exit: ")
+    if reuse.lower()  == "no":
+        again = False
+    elif reuse.lower() == "yes":
+        again = True
+    else:
+        again = False
+        print("Invaild ")   

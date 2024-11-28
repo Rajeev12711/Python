0e664354fd3a5433b598a04179c8214936d2f19a
Rajeev <126416818+rajeev-27@users.noreply.github.com>
2024-11-28 21:59:44 +0530

    Python

--- Day-10/exercise/main2.py
+++ Day-10/exercise/main2.py
@@ -6,7 +6,7 @@ def multiple(x, y):
     return x*y
 def divide(x, y):
     if y == 0:
-        return "Error: Division by zero is not allowed."
+        return
     return x / y
 def module(x, y):
     return x%y
@@ -19,24 +19,32 @@ operations ={
     "%": module
     }
 
-x = float(input("enter the first number: "))
-again = True
-while again == True:
-    for operator in operations:
-        print(operator)
-    symbol = input("Choose Any Operation From the Line Above: ")
-    y = float(input("enter the next number: "))
-    calculate = operations[symbol]
-    answer = calculate(x, y)
+def  calculater():
+    print("Welcome to world of Calculation")
+    x = float(input("enter the first number: "))
+    again = True
+    while again == True:
+        for operator in operations:
+            print(operator)
+        symbol = input("Choose Any Operation From the Line Above: ")
+        y = float(input("enter the next number: "))
+        calculate = operations[symbol]
+        answer = calculate(x, y)
 
-    print(f"{x} {symbol} {y} = {answer}")
-    x = answer
-    
-    reuse = input("Type 'yes' to continue calculating with  {answer}, or type 'no' to Exit: ")
-    if reuse.lower()  == "no":
-        again = False
-    elif reuse.lower() == "yes":
-        again = True
-    else:
-        again = False
-        print("Invaild ")   
+        print(f"{x} {symbol} {y} = {answer}")
+        x = answer
+        
+        reuse = input(f"Type 'yes' to continue calculating with  {answer}, or type 'no' to Start new calculation, or type 'Exit' for Exit the calculater: ")
+        if reuse.lower()  == "no":
+            again = False
+            calculater()
+        elif reuse.lower() == "yes":
+
+            again = True
+        elif reuse.lower() == "exit":
+            again =False
+        else:
+            again = False
+            print("Invaild ")
+
+calculater() 

c0df8ebb4e1a933c90fc31ab170f5588894802c1
Rajeev <126416818+rajeev-27@users.noreply.github.com>
2024-12-02 00:00:09 +0530

    Python

--- capstone-project/main.py
+++ capstone-project/main.py
@@ -6,24 +6,27 @@ def game():
     card = random.choice(cards)
     return card
 
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) ==2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
 user = []
 computer = []
+game_over = False
 for _ in range(0, 2):
     user.append(game())
     computer.append(game())
 
-print(computer, user)
+user_score = calculate_score(user)
+computer_score = calculate_score(computer)
+print(f" your card: {user}, current score: {user_score}")
+print(f" computer first card: {computer[0]}")
+if user_score == 0 or computer_score == 0 or user_score>21:
+    game_over=True
 
-
-def calculate_score(cards):
-    return sum(cards)
-
-score=calculate_score(cards)
-print(score)
-
-        
-
-
-
-
-
+    
+    

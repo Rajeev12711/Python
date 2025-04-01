235131b6902dcf521a1467b9a7487b8b48edde47
Rajeev <rajeev782772@gmail.com>
2025-04-01 10:45:02 +0530

    Python

---
+++ Day-22/Final Turtle Crossing Game/scoreborad.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+
+FONT = ("Courier", 24, "normal")
+
+class Score(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.penup()
+        self.hideturtle()
+        self.goto(-250, 250 )
+        self.write(f"Level: {self.level}", font=FONT)
+
+    def level_up(self):
+        self.clear()
+        self.level +=1
+        self.write(f"Level: {self.level}", font=FONT)

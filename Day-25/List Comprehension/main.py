e045f6804a75ad440a14f7c441d52c453f9f7143
Rajeev <rajeev782772@gmail.com>
2025-04-11 23:22:49 +0530

    Python

---
+++ Day-25/List Comprehension/main.py
@@ -0,0 +1,10 @@
+numbers = [1,2,3,4,5]
+new_numbers =[i +1 for i in numbers]
+print(new_numbers)
+
+name = "Rajeev"
+letter_list = [letter for letter in name]
+print(letter_list)
+
+list_of_table=[number*2 for number in range(1, 5)]
+print(list_of_table)
